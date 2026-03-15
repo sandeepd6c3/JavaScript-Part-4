8bd1e475b1426a09bbcbdc5e997018fd42f1078b
sandeepd6c3 <sandeep977244@gmail.com>
2026-03-15 23:04:10 +0530

    Factory Function DesignPatterns

--- DesignPatterns/script.js
+++ DesignPatterns/script.js
@@ -1,45 +1,79 @@
 //  -------- Module Patterns ---------
 
-let Bank = (function () {
+// let Bank = (function () {
 
-    let Balance = 100000;
+//     let Balance = 100000;
 
-    function checkBalance() {
-        console.log(Balance);
+//     function checkBalance() {
+//         console.log(Balance);
 
-    }
-    function setBalance(val) {
-        Balance = val;
+//     }
+//     function setBalance(val) {
+//         Balance = val;
 
-    }
- 
-    function withdraw(val) {
-        if (val <= Balance) {
-            Balance -= val;
-            console.log(Balance);
+//     }
 
-        }
-    }
+//     function withdraw(val) {
+//         if (val <= Balance) {
+//             Balance -= val;
+//             console.log(Balance);
+
+//         }
+//     }
 
 // Module Patterns
-    // return {
-    //     checkBalance,
-    //     setBalance,
-    //     withdraw,
-    // };
+// return {
+//     checkBalance,
+//     setBalance,
+//     withdraw,
+// };
 
-    // ------- Revealing Module Pattern ------
-    return {
-        check:checkBalance,
-        set:setBalance,
-        wid:withdraw,
-    }
-})();
+// ------- Revealing Module Pattern ------
+//     return {
+//         check:checkBalance,
+//         set:setBalance,
+//         wid:withdraw,
+//     }
+// })();
 
 // Bank.checkBalance();
 // Bank.withdraw(50000);
 // Bank.checkBalance();
 
-Bank.check()
-Bank.set(100000)
-Bank.check()
+// Bank.check()
+// Bank.set(100000)
+// Bank.check()
+
+
+
+//    --------=> Factory Function Pattern <=--------
+
+function Factory(name, price) {
+
+    let stock = 20;
+    return {
+        name,
+        price,
+        buy(qty) {
+            if (qty < stock) {
+                stock -= qty;
+                console.log(`${qty} Booked , ${stock} Pices left `);
+
+            }
+            else {
+                console.log(`only ${stock} Pices in stock`);
+
+            }
+        },
+        refil(qty) {
+            stock += qty;
+            console.log(`${stock} pices in stock`);
+
+        }
+    }
+
+}
+
+let mobile = Factory("iphone", 80000);
+
+mobile.refil(80);
